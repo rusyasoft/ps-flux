b49886ea927ff949d765dcea3e433c2fe157c904
rusyasoft <rusyasoft@gmail.com>
2022-09-02 20:19:34 -0700

    Form has been added to interact with react

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { Prompt } from 'react-router-dom';
+import CourseForm from "./CourseForm";
 
 const ManageCoursePage = props => {
+    const [ course, setCourse ] = useState({
+        id: null,
+        slug: "",
+        title: "",
+        authorId: null,
+        category: ""
+    });
+
+    function handleChange({target}) {
+        const updatedCourse = {...course, [target.name]: target.value};
+        setCourse(updatedCourse);
+    }
+
     return (
         <>
             <h2>Manage Course</h2>
+            <CourseForm course={course} onChange={handleChange}/>
             <Prompt when={true} message="Are you sure you want to leave" />
             { props.match.params.slug }
         </>
